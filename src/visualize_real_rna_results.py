5121d7d54ef4c48861ac2cba90a925f41bcd9d25
Max Ward <max@DESKTOP-S0P2PVG>
2022-11-23 12:48:07 +0800

    Updates the result visualisation for real rna.

--- src/visualize_real_rna_results.py
+++ src/visualize_real_rna_results.py
@@ -1,33 +1,34 @@
-# Usage is to run python3 src/visualize_real_rna_results.py < results_from_benchmark_fit_fns_on_real_rna_dot_py
+# Usage is python3 src/visualize_real_rna_results.py < results/real_rna_bench
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.font_manager as font_manager
 
 def main():
-    pr = np.zeros(101)
-    ed = np.zeros(101)
-    bp = np.zeros(101)
-    hd = np.zeros(101)
+    buckets = 100
+    pr = np.zeros(buckets+1)
+    ed = np.zeros(buckets+1)
+    bp = np.zeros(buckets+1)
+    hd = np.zeros(buckets+1)
     for ln in sys.stdin:
-        if ln.startswith("Result"):
+        if ln.startswith("Result") or ln.find(":") != -1:
             break
         name, lst = ln.strip().split('[')
         lst = "[" + lst
         lst = eval(lst)
         if lst[0] is None:
             continue
-        pr[round(lst[0]*100)] += 1
-        ed[round(lst[1]*100)] += 1
-        bp[round(lst[2]*100)] += 1
-        hd[round(lst[3]*100)] += 1
+        pr[round(lst[0]*buckets)] += 1
+        ed[round(lst[1]*buckets)] += 1
+        bp[round(lst[2]*buckets)] += 1
+        hd[round(lst[3]*buckets)] += 1
     x_axis = np.arange(len(pr))
 
-    p = plt.plot(x_axis, np.cumsum(pr), color='b')
-    e = plt.plot(x_axis, np.cumsum(ed), color='r')
-    s = plt.plot(x_axis, np.cumsum(hd), color='g')
+    p, = plt.plot(x_axis, np.cumsum(pr), color='b')
+    e, = plt.plot(x_axis, np.cumsum(ed), color='r')
+    s, = plt.plot(x_axis, np.cumsum(hd), color='g')
     font = {'fontname':'serif'}
-    plt.legend((p, e, s), ('Probability', 'Ensemble Defect' 'Structure Distance'), prop=font_manager.FontProperties(family='serif'))
+    plt.legend((p, e, s), ('Probability', 'Ensemble Defect', 'Structure Distance'), prop=font_manager.FontProperties(family='serif'))
 
     plt.xlabel('Relative Rank Percentile', fontdict=font)
     plt.ylabel('Cumulative Number of RNAs', fontdict=font)
